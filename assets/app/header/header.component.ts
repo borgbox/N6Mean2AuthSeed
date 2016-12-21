7175ce4f37023ac11efaacf018612f0e7831af21
franciscofreitas2001 <franciscofreitas2001@gmail.com>
2016-12-21 21:49:13 -0200

    Ajuste link logout

--- assets/app/header/header.component.ts
+++ assets/app/header/header.component.ts
@@ -22,7 +22,7 @@ export class HeaderComponent implements OnInit {
 
     logout() {
         this.authorizationService.logout();
-        this.router.navigateByUrl('/login');
+        //this.router.navigateByUrl('/login');
     }
 
 }
